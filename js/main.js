cdec0f4e2b163af07ca71643d93e7639e76dfd6d
kkowalska <kingaannamaria@gmail.com>
2022-11-09 10:34:55 +0100

    zadanie

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ console.log(number);
 
 if (number > 10) {
     console.log("Podana liczba jest większa od 10");
-} else if (number < 10) {
+} else  {
     console.log("Podana liczba nie jest większa od 10");
 }
